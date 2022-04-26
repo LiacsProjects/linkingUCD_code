63984930c2df3e507cc336e4adffa53fbb902437
Rickie1000 <rickie.schreuder@gmail.com>
2022-04-26 13:44:41 +0200

    added hoogleraren csv

--- Database/Python/connect.py
+++ Database/Python/connect.py
@@ -1,4 +1,17 @@
 import mysql.connector
+import pandas as pd
+from faker import Faker
+from collections import defaultdict
+from sqlalchemy import create_engine
+fake = Faker()
+fake_data = defaultdict(list)
+for _ in range(1000):
+  fake_data["Firstname"].append(fake.first_name())
+  fake_data["Lastname"].append(fake.last_name())
+
+df_fake_data = pd.DataFrame(fake_data)
+
+print(df_fake_data)
 
 mydb = mysql.connector.connect(
   host="localhost",
@@ -12,7 +25,7 @@ mycursor = mydb.cursor()
 
 sql = "INSERT INTO person (PersonID, Firstname, Lastname) VALUES (%s, %s, %s)"
 val = [
-  (1, 'pieter', 'hendriks'),
+  (),
 ]
 
 mycursor.executemany(sql, val)
