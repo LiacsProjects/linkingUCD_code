6f48fad405347608c55ea7e46b8308af1ef04241
Michael de Koning <michael.de.koning@live.nl>
2022-05-05 14:24:46 +0200

    Updated use of cursors

--- connectAdapter.py
+++ connectAdapter.py
@@ -5,39 +5,60 @@ import numpy as np
 from collections import defaultdict
 from sqlalchemy import create_engine
 
+# Connect to database
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="LinkingUCD",
+    password="UCDAdapter",
+    database="test_via_eer",
+    port="3306"
+)
 
-def checkIfTableExists(cursor, table_to_check):
+
+# Return True if table exists, else false
+def checkIfTableExists(table_to_check):
+    cursor = mydb.cursor()
     cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'test_via_eer'")
     tables = cursor.fetchall()
 
     for table in tables:
         if table_to_check == str(table)[2:str(table).find('\',')]:
+            cursor.close()
             return True
     print("Table does not exist")
+    cursor.close()
     return False
 
 
-def select(s_cursor):
-    s_cursor.execute("SELECT * FROM person")
+def select(table_name):
+    s_cursor = mydb.cursor()
+    s_cursor.execute("""SELECT * FROM %s""" % table_name)
     result = s_cursor.fetchall()
     print("SELECT:")
     for person in result:
         print(person)
+    s_cursor.close()
 
 
-def insert(i_cursor, table_name, val):
+# val is data from excel file
+def insert(table_name, val):
+    i_cursor = mydb.cursor()
     # Check if entry already exists, else UPDATE?
     sql = """INSERT INTO %s (idPerson, Firstname, Lastname, Pob, Pod, Gender, Dob, Dod) VALUES (%%s, %%s, %%s, %%s, %%s, %%s, %%s, %%s)"""
     i_cursor.executemany(sql % table_name, val)
+    i_cursor.close()
 
 
-def deleteTableAll(d_cursor, table_name):
-    # Add check if table exists
-    if checkIfTableExists(d_cursor, table_name):
+# Delete all contents of table without removing table
+def deleteTableAll(table_name):
+    if checkIfTableExists(table_name):
+        d_cursor = mydb.cursor()
         sql = """DELETE FROM %s"""
         d_cursor.execute(sql % table_name)
+        d_cursor.close()
 
 
+# Remove dates that do not match the format
 def fixDates(df, name):
     for column in name.split(', '):
         for Dod in df[column]:
@@ -50,6 +71,7 @@ def fixDates(df, name):
     return df
 
 
+# main
 if __name__ == '__main__':
     # using openpyxl for .xlsx files
     excel_full = pd.read_excel(r"Hoogleraren all.xlsx", engine='openpyxl')
@@ -61,25 +83,10 @@ if __name__ == '__main__':
     df_person = df_person.replace({np.nan: None})
     df_person = fixDates(df_person, "Sterfdatum, geboortedatum")
 
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="LinkingUCD",
-        password="UCDAdapter",
-        database="test_via_eer",
-        port="3306"
-    )
-
-    mycursor = mydb.cursor()
-    select(mycursor)
-
-    deleteTableAll(mycursor, "person")
-    insert(mycursor, "person", list(df_person.itertuples(index=False, name=None)))
-
-    select(mycursor)
-
-    # deleteTableAll(mycursor, "person")
-
-    # select(mycursor)
+    # deleteTableAll("person")
+    # select("person")
+    insert("person", list(df_person.itertuples(index=False, name=None)))
+    select("person")
 
     mydb.commit()
     mydb.close()
