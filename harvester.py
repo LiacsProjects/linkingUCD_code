bf5a8fb9a0eb9be5d06d0aaa70d1169cf63254f7
Michael de Koning <michael.de.koning@live.nl>
2022-05-12 16:05:54 +0200

    Added cases for all registers

--- harvester.py
+++ harvester.py
@@ -4,7 +4,7 @@ from sickle import Sickle
 URL = 'https://webservices.picturae.com/a2a/20a181d4-c896-489f-9d16-20a3b7306b15/'
 
 '''
-0 - Borgbrieven
+0 - Borgbrieven, 4.206 records
 1 - Bonboeken
 2 - Bevolkingsregister, 292.758 records
 3 - BS Echtscheidingsakte
@@ -24,31 +24,66 @@ URL = 'https://webservices.picturae.com/a2a/20a181d4-c896-489f-9d16-20a3b7306b15
 
 
 # Harvest data per set
-def sickleHarvest():
+def sickleHarvest(register_number):
+    if register_number < 0 or register_number > 13:
+        print("Invalid register number")
+        return
+
     sickle = Sickle(URL)
     register_set = ['bb_a', 'bn_a', 'br_a', 'bs_e', 'bs_g', 'bs_h', 'bs_o', 'dtb_b', 'dtb_d', 'dtb_t', 'ml_a', 'na_a',
                     'po_a', 'ra_a']
-    records = sickle.ListRecords(metadataPrefix='a2a', set=register_set[9])
+    records = sickle.ListRecords(metadataPrefix='a2a', set=register_set[register_number])
 
     count = file_count = 0
-    file_location_base = 'data/Erfgoed Leiden/DTBTrouwregister_records'
+    if register_number is 0:
+        file_location_base = 'data/Erfgoed Leiden/Borgbrieven_records'
+    elif register_number is 1:
+        file_location_base = 'data/Erfgoed Leiden/Bonboeken_records'
+    elif register_number is 2:
+        file_location_base = 'data/Erfgoed Leiden/Bevolkingsregister_records'
+    elif register_number is 3:
+        file_location_base = 'data/Erfgoed Leiden/BS Echtscheidingsakte_records'
+    elif register_number is 4:
+        file_location_base = 'data/Erfgoed Leiden/BS Geboorte_records'
+    elif register_number is 5:
+        file_location_base = 'data/Erfgoed Leiden/BS Huwelijk_records'
+    elif register_number is 6:
+        file_location_base = 'data/Erfgoed Leiden/BS Overlijden_records'
+    elif register_number is 7:
+        file_location_base = 'data/Erfgoed Leiden/DTB Begraven_records'
+    elif register_number is 8:
+        file_location_base = 'data/Erfgoed Leiden/DTB Dopen_records'
+    elif register_number is 9:
+        file_location_base = 'data/Erfgoed Leiden/DTB Trouwen_records'
+    elif register_number is 10:
+        file_location_base = 'data/Erfgoed Leiden/Militieregisters_records'
+    elif register_number is 11:
+        file_location_base = 'data/Erfgoed Leiden/NA_records'
+    elif register_number is 12:
+        file_location_base = 'data/Erfgoed Leiden/Poorterboeken_records'
+    elif register_number is 13:
+        file_location_base = 'data/Erfgoed Leiden/Oud Rechterlijke Akten_records'
+    else:
+        return
+
     while True:
         file_count = file_count + 1
         file_location = file_location_base + str(file_count) + '.xml'
         with open(file_location, 'w', encoding="utf-8") as fp:
             # Assume at least one record is present
-            temp = records.next().raw
+            record = records.next().raw
             # Limit #records to 300.000 per file
             for i in range(300000):
                 count = count + 1
                 print(count)
-                temp = temp[:7] + str(count) + temp[7:]
-                temp = temp[:temp.find("/record") + 7] + str(count) + temp[temp.find("/record") + 7:]
-                fp.write(temp + '\n')
+                record = record[:7] + str(count) + record[7:]
+                record = record[:record.find("/record") + 7] + str(count) + record[record.find("/record") + 7:]
+                fp.write(record + '\n')
                 try:
-                    temp = records.next().raw
+                    record = records.next().raw
                 except StopIteration:
-                    print("Successfully retrieved all records from \"{}\" set".format(register_set))
+                    print("Successfully retrieved all " + str(count) + " records from \"{}\" set".format(
+                        register_set[register_number]))
                     fp.close()
                     return
             fp.close()
@@ -56,4 +91,5 @@ def sickleHarvest():
 
 # Main
 if __name__ == '__main__':
-    sickleHarvest()
+    sickleHarvest(0)
+    sickleHarvest(1)
