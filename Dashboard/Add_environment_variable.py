d5ed111588a341c8ea92f6ee5f4761610986ffea
DESKTOP-R9M5AU2\benva <ben.vanyperen@kpnmail.nl>
2023-04-04 13:15:12 +0200

    add environment aangepast

--- Dashboard/Add_environment_variable.py
+++ Dashboard/Add_environment_variable.py
@@ -4,11 +4,16 @@
   
 # importing os module 
 import os
-  
-# Add a new environment variable 
-# os.environ['DASHBOARD_BASEPATH'] = 'c:/users/Benva/Jupyter/Dash applications/Linking UCD - dashboard/'
-os.environ['DASHBOARD_BASEPATH'] = 'C:/Users/micha/Documents/Leiden/Univercity/linkingUCD_code/Dashboard/'
+import sys
+import platform
+
+if os.name == "nt":
+    # Add a new environment variable
+    os.environ['DASHBOARD_BASEPATH'] = 'c:/users/Benva/PycharmProjects/linkingUCD_code/Dashboard/'
+    #os.environ['DASHBOARD_BASEPATH'] = 'C:/Users/micha/Documents/Leiden/Univercity/linkingUCD_code/Dashboard/'
 
 # Get the value of
-# Added environment variable 
-print("DASHBOARD_BASEPATH", os.environ['DASHBOARD_BASEPATH'])
+# Added environment variable
+print("Operating System:", platform.platform())
+print("Python version:", sys.version)
+print("DASHBOARD_BASEPATH:", os.environ['DASHBOARD_BASEPATH'])
